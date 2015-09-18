46839689a0591de095190aacdd7ef99302da7d37
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-09-18 09:52:17 +0400

    Saving work

--- startDay2.sh
+++ startDay2.sh
@@ -1,22 +1,22 @@
 docker stop logspout kibana logstash es
 docker rm logspout kibana logstash es
-docker run -it -d \
+docker run -d \
     -p 9200:9200 \
     -p 9300:9300 \
     --name es \
-    ehazlett/elasticsearch
-docker run -it -d \
+    elasticsearch
+docker run -d \
     -p 5000:5000 \
     -p 5000:5000/udp \
     -v $(pwd)/logstash/logstash.conf:/etc/logstash.conf \
     --link es:elasticsearch \
     --name logstash \
-    ehazlett/logstash -f /etc/logstash.conf
+    logstash -f /etc/logstash.conf
 export LOGSTASH=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' logstash)
-docker run -it -d \
+docker run -d \
     -p 80:80 \
     --name kibana \
-    ehazlett/kibana
+    kibana
 echo $LOGSTASH
 docker run -d \
     -v /var/run/docker.sock:/tmp/docker.sock \
