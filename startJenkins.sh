d0fb8261161daf22ae4ea56314cf7c3125f58776
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-09-21 06:25:05 +0530

    Add data container

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,1 +1,2 @@
-docker run --name jenkins -v /var/jenkins_home -d -p 8080:8080 jenkins
+docker run -v /var/jenkins_home --name jenkins-data busybox
+docker run --volumes-from jenkins-data --name jenkins -v /var/jenkins_home -d -p 8080:8080 jenkins
