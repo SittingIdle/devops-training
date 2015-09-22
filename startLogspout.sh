26a1ea3549f9c75b0752a13bbf2db0b396992b79
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-09-22 08:34:13 +0530

    Make Papertrail dynamic

--- startLogspout.sh
+++ startLogspout.sh
@@ -1,1 +1,7 @@
-docker run --name logspout --hostname=log-devops-training --restart=always -d -v=/var/run/docker.sock:/tmp/docker.sock progrium/logspout syslog://logs3.papertrailapp.com:29257
+docker run \
+--name logspout \
+--hostname=log-devops-training \
+--restart=always -d \
+-v=/var/run/docker.sock:/tmp/docker.sock \
+progrium/logspout \
+syslog://$PAPERTRAIL
