644f5989c8aa7df897bc7bf8cc975f0d3355bd55
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-10-27 09:56:44 -0400

    Create startNginx.sh

---
+++ bash/startNginx.sh
@@ -0,0 +1,12 @@
+docker rm nginx
+docker run -d \
+          -v nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+          --name nginx \
+          -p 80:80 \
+          --link xld \
+          --link xlr \
+          --link jenkins \
+          --link mocha \
+          --link wildfly-qa \
+          --link wildfly-pr \
+          nginx
