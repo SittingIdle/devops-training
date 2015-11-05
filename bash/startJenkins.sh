aaaa76af214dc87dea3c8a0e1526872184eeaf54
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-11-05 08:53:03 +0530

    Update startJenkins.sh

--- bash/startJenkins.sh
+++ bash/startJenkins.sh
@@ -1,8 +1,8 @@
-docker create --name jenkins-data jenkins
+docker create --name jenkins-data jenkins:1.609.3
 docker run -d \
           --volumes-from jenkins-data \
           --name jenkins \
           -p 8080:8080 \
           --link xld \
           --link xlr \
-          jenkins
+          jenkins:1.609.3
