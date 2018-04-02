0082ff893b27694da9355d801f813998d44c04bb
ankur07garg <ankur.tcs@gmail.com>
2018-04-02 17:57:29 +0530

    Update startJenkins.sh

--- bash/startJenkins.sh
+++ bash/startJenkins.sh
@@ -1,9 +1,9 @@
-docker create --name jenkins-data jenkins:2.60.3
+docker create --name jenkins-data jenkins:2.107.1
 docker run -d \
           --volumes-from jenkins-data \
           --name jenkins \
           -p 8080:8080 \
-          jenkins:2.60.3
+          jenkins:2.107.1
 
          # --link xld \
          # --link xlr \
