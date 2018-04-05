24ec71592bb26d78f4308c5f6d63654be3146ca7
EC2 Default User <ec2-user@ip-172-31-41-102.ec2.internal>
2018-04-05 10:43:16 +0000

    plugins

--- bash/docker/jenkins/plugins.sh
+++ bash/docker/jenkins/plugins.sh
@@ -3,9 +3,9 @@
 # Parse a support-core plugin -style txt file as specification for jenkins plugins to be installed
 # in the reference directory, so user can define a derived Docker image with just :
 #
-# FROM jenkins
-# COPY plugins.txt /plugins.txt
-# RUN /usr/local/bin/plugins.sh /plugins.txt
+ FROM jenkins
+ COPY plugins.txt /plugins.txt
+ RUN /usr/local/bin/plugins.sh /plugins.txt
 #
 
 set -e
