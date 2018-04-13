85d006ce1136a4a6ec8dbbdbf3754e4dac9b1ff5
Ankur Garg <ankur.tcs@gmail.com>
2018-04-13 10:06:10 +0000

    changes to expose admin port, minor changes

--- bash/startWildflyQA.sh
+++ bash/startWildflyQA.sh
@@ -1,6 +1,6 @@
-docker create --name wildfly-qa-data adejonge/wildfly
+docker create --name wildfly-qa-data agarg/wildfly
 docker run -d \
           --volumes-from wildfly-qa-data \
           --name wildfly-qa \
-          -p 1022:22 -p 8081:8081 \
-          adejonge/wildfly
+	  -p 1022:22 -p 8081:8081 -p 9990:9990 \
+          agarg/wildfly
